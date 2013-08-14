262f51006a24df8a9f8a76e4d0959b50b5453db0
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2013-08-14 15:41:21 +1000

    Generated by generator-bespoke

--- scripts/main.js
+++ scripts/main.js
@@ -13,4 +13,25 @@
     state: true
 	});
 
+  (function preloadBackgroundImages() {
+
+    var matches, image,
+      forEach = function(arrayLike, fn) {
+        [].slice.call(arrayLike, 0).forEach(fn);
+      };
+
+    forEach(document.styleSheets, function(sheet) {
+      forEach(sheet.rules, function(rule) {
+        if (rule.style && rule.style.backgroundImage) {
+          matches = rule.style.backgroundImage.match(/url\((.*)\)/);
+          if (matches) {
+            image = new Image();
+            image.src = matches[1];
+          }
+        }
+      });
+    });
+
+  }());
+
 }());
